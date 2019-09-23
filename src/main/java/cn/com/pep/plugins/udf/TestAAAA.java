4dc51e36bab0dea40cc90557fc4f5641e72ba0b1
QinZhen <1181047207@qq.com>
2019-09-23 17:33:17 +0800

    加到本地仓库哦

---
+++ src/main/java/cn/com/pep/plugins/udf/TestAAAA.java
@@ -0,0 +1,14 @@
+package cn.com.pep.plugins.udf;
+
+/**
+ * @Description:
+ * @author:QZ
+ * @date:2019/9/23 17:32
+ */
+public class TestAAAA {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello World!!");
+    }
+}
